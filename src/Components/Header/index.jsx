1b7968b5e3b52ffa075d49d990b585c8a1944a81
devgugga <gustavogomes90104@gmail.com>
2024-12-01 15:43:09 -0300

    💄 Header Component
    Neste commit foi criado o component header da aplicação.

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import styles from './Header.module.css'
-import { Link } from 'react-router-dom'
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import Dog from "../../Assets/dogs.svg?react";
 
 export default function Header() {
   return (
-    <div className={styles.header}>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dog />
+        </Link>
+        <Link className={styles.login} to="/">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
-  )
+    </header>
+  );
 }
