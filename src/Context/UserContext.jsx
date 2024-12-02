6979bbdf6c1f1c38c666e0d1bdad982e74ea9e09
devgugga <gustavogomes90104@gmail.com>
2024-12-02 15:13:05 -0300

    ✨ User Context
    Neste commit foi finalizada a logica do UserContext, foi adicionado validações de erro, verificação para ver se o usuário já esta logado se tiver enviar ele para a pagina de conta.

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect } from "react";
-import { TOKEN_POST, USER_GET } from "../Utils/api";
+import { createContext, useState, useEffect, useCallback } from "react";
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from "../Utils/api";
+import { useNavigate } from "react-router-dom";
 
 export const UserContext = createContext();
 
@@ -8,6 +9,46 @@ export function UserStorage({ children }) {
   const [login, setLogin] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const userLogout = useCallback(
+    async function () {
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+
+      window.localStorage.removeItem("token");
+
+      navigate("/login");
+    },
+    [navigate]
+  );
+
+  async function userLogin(username, password) {
+    try {
+      setError(null);
+      setLoading(true);
+
+      const { url, options } = TOKEN_POST({ username, password });
+      const response = await fetch(url, options);
+
+      if (!response.ok) throw new Error(`Error: ${response.statusText}`);
+
+      const { token } = await response.json();
+
+      window.localStorage.setItem("token", token);
+
+      await getUser(token);
+
+      navigate("/conta");
+    } catch (err) {
+      setError(err.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
@@ -18,18 +59,34 @@ export function UserStorage({ children }) {
     setLogin(true);
   }
 
-  async function userLogin(username, password) {
-    const { url, options } = TOKEN_POST({ username, password });
-    const response = await fetch(url, options);
-    const { token } = await response.json();
+  useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem("token");
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
 
-    window.localStorage.setItem("token", token);
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+          const response = await fetch(url, options);
 
-    getUser(token);
-  }
+          if (!response.ok) throw new Error("Token invalido");
+
+          await getUser(token);
+        } catch {
+          userLogout();
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, [userLogout]);
 
   return (
-    <UserContext.Provider value={{ userLogin, data }}>
+    <UserContext.Provider
+      value={{ userLogin, userLogout, data, error, loading, login }}
+    >
       {children}
     </UserContext.Provider>
   );
--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,10 +1,16 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import LoginForm from "../../Components/Login/LoginForm";
 import LoginCreate from "../../Components/Login/LoginCreate";
 import LoginPasswordLost from "../../Components/Login/LoginPasswordLost";
 import LoginPasswordReset from "../../Components/Login/LoginPasswordReset";
+import { useContext } from "react";
+import { UserContext } from "../../Context/UserContext";
 
 export default function Login() {
+  const { login } = useContext(UserContext);
+
+  if (login) return <Navigate to="/conta" />;
+
   return (
     <div>
       <Routes>
