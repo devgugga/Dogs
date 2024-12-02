54b1ca45539a9652a10f5d42fa763959ca86d2b3
devgugga <gustavogomes90104@gmail.com>
2024-12-01 23:15:19 -0300

    ✨ Custom Hooks
    Neste commit foi criado um hook personalizado para a validação de formulários.

--- src/Components/Forms/Input/index.jsx
+++ src/Components/Forms/Input/index.jsx
@@ -1,13 +1,29 @@
 import styles from "./Input.module.css";
 
-export default function Input({ label, type, name }) {
+export default function Input({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  error,
+  onBlur,
+}) {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} name={name} className={styles.input} type={type} />
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 }
