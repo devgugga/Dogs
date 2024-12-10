47095c9c1d5ac5a904648033c57bab44553540ba
devgugga <gustavogomes90104@gmail.com>
2024-12-10 11:36:29 -0300

    :sparkles: User Statistic
    Neste commit foi criado a interface para ver as estatistica dos usuários.

--- src/Components/User/UserStats/index.jsx
+++ src/Components/User/UserStats/index.jsx
@@ -1,5 +1,33 @@
 import { Head } from "../../Helpers/Head.jsx";
+import { useFetch } from "../../../Hooks/useFetch.jsx";
+import { useEffect } from "react";
+import { STATS_GET } from "../../../Utils/api.js";
+import { Loading } from "../../Helpers/Loading/index.jsx";
+import Error from "../../Helpers/Error/index.jsx";
+import { UserStatsGraphs } from "../UserStatsGraphs/index.jsx";
 
 export function UserStats() {
-  return <Head title="Estastíticas" />;
+  const { data, error, loading, request } = useFetch();
+
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET(token);
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data) {
+    return (
+      <>
+        <Head title="Estastíticas" />
+        <UserStatsGraphs data={data} />
+      </>
+    );
+  } else return null;
 }
