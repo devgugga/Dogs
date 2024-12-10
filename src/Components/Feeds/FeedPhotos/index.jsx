13107da3e883a97886e5019baa9d11a0caf73350
devgugga <gustavogomes90104@gmail.com>
2024-12-10 07:50:02 -0300

    :sparkles: Infinity Scroll
    Neste commit foi criado a funcionalidade de scroll infinito.

--- src/Components/Feeds/FeedPhotos/index.jsx
+++ src/Components/Feeds/FeedPhotos/index.jsx
@@ -6,16 +6,24 @@ import Error from "../../Helpers/Error/index.jsx";
 import { Loading } from "../../Helpers/Loading/index.jsx";
 import styles from "./FeedPhotos.module.css";
 
-export function FeedPhotos({ setModalPhoto }) {
+export function FeedPhotos({ user, page, setModalPhoto, setInfinite }) {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const total = 6;
+
+      const { url, options } = PHOTOS_GET({
+        page: page,
+        total: total,
+        user: user,
+      });
       const { response, json } = await request(url, options);
+
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
--- src/Pages/User/index.jsx
+++ src/Pages/User/index.jsx
@@ -3,13 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import { Feed } from "../../Components/Feeds/Feed/index.jsx";
 import { UserPhotoPost } from "../../Components/User/UserPhotoPost/index.jsx";
 import { UserStats } from "../../Components/User/UserStats/index.jsx";
+import { useContext } from "react";
+import { UserContext } from "../../Context/UserContext.jsx";
 
 export function User() {
+  const { data } = useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
       </Routes>
